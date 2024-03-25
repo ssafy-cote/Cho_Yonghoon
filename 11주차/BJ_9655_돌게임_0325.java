a3ce66dee946f8f8a78fcade161d7290bc61f9f1
조용훈 <sjsin0905@naver.com>
2024-03-25 20:22:27 +0900

    문제풀이 0325

---
+++ 11주차/BJ_9655_돌게임_0325.java
@@ -0,0 +1,16 @@
+package algo0325;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_9655_돌게임_0325 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+		
+		
+	}
+}
